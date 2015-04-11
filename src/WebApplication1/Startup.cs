c2ed9ffa7244b5ec760695d897925009602d9982
darkcrash <dark_crash@hotmail.com>
2015-04-11 20:04:03 +0900

    6

--- src/WebApplication1/Startup.cs
+++ src/WebApplication1/Startup.cs
@@ -79,10 +79,11 @@ namespace WebApplication1
             // Add MVC to the request pipeline.
             app.UseMvc(routes =>
             {
+
                 routes.MapRoute(
                     name: "default",
                     template: "{controller}/{action}/{id?}",
-                    defaults: new { controller = "Home", action = "Index" });
+                    defaults: new { controller = "Default", action = "Index" });
 
                 // Uncomment the following line to add a route for porting Web API 2 controllers.
                 // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
