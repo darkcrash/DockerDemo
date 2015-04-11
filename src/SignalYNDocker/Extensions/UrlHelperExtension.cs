057c28e24cac5270856d751ab530cdd753743a8f
darkcrash <dark_crash@hotmail.com>
2015-04-11 19:59:51 +0900

    2

---
+++ src/SignalYNDocker/Extensions/UrlHelperExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet;
+using Microsoft.AspNet.Mvc;
+
+namespace SignalYN
+{
+    public static class UrlHelperExtension
+    {
+        public static string AppUrl(this IUrlHelper urlHelper)
+        {
+            //var request = urlHelper.RequestContext.HttpContext.Request;
+            var request = System.Web.HttpContext.Current.Request;
+            return request.Url.GetLeftPart(UriPartial.Scheme | UriPartial.Authority);
+        }
+    }
+}
